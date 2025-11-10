65e221ac74bb6632c19ddbee5b1fa22744b80282
Bima0195 <abimaelsouza2001@gmail.com>
2025-11-10 00:03:55 -0300

    Add files via upload

---
+++ LaboratorioIII/coffee/src/main/java/model/Cafe.java
@@ -0,0 +1,42 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cafe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    private String tipo;
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
+
